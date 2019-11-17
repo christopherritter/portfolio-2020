a14bd39189c431659c051238a4fad790881a7b6e
Christopher Ritter <chris@christopherritter.com>
2019-11-17 07:38:50 -0500

    removed lint plugin / added vue-analytics

--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import BootstrapVue from "bootstrap-vue";
-
 import store from "./store";
+import BootstrapVue from "bootstrap-vue";
+import VueAnalytics from "vue-analytics";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+
+const isProd = process.env.NODE_ENV === 'production';
+
+Vue.use(VueAnalytics, {
+  id: "UA-139579603-1",
+  router,
+  debug: {
+    enabled: !isProd,
+    sendHitTask: isProd
+  }
+});
 
 new Vue({
   router,
